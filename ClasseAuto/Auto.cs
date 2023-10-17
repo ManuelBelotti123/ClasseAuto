68188f92bb3e79ffa2c997a83abdb858635f5282
Belotti.20181 <Belotti.20181@E2-9-PC19>
2023-10-17 13:03:14 +0200

    Classe Auto, attributi, proprietà, costruttori

--- ClasseAuto/Auto.cs
+++ ClasseAuto/Auto.cs
@@ -3,11 +3,50 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolTip;
 
 namespace ClasseAuto
 {
     internal class Auto
     {
+        //attributi
+        private bool _motore;
+        private double _velocita;
+        private int _marcia;
+
+        //proprietà
+        public bool Motore
+        {
+            get { return _motore; }
+            set { _motore = value; }
+        }
+        public double Velocita
+        {
+            get { return _velocita; }
+            set { _velocita = value; }
+        }
+        public int Marcia
+        {
+            get { return _marcia; }
+            set { _marcia = value; }
+        }
+
+        //costruttori
+        public Auto(bool mot, double vel, int mar)
+        {
+            Motore = mot;
+            Velocita = vel;
+            Marcia = mar;
+        }
+        public Auto()
+        {
+            Motore = false;
+            Velocita = 0;
+            Marcia = 0;
+        }
+
+        //metodi
+
 
     }
 }
